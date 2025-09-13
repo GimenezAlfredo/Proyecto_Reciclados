0a3e3d74d0e0045991bdddbd6fd79e7147f38648
GimenezAlfredo <alfredogimenez431@gmail.com>
2025-09-13 19:47:49 -0300

    configuracion-deploy-1

--- Back-end/src/index.ts
+++ Back-end/src/index.ts
@@ -10,10 +10,15 @@ dotenv.config()
 const app = express()
 
 //cors bien configurado
+// app.use(cors({
+//   origin: 'http://localhost:5173', //luego cambiar a HTTPS y el nombre del domisnio 'https://midominio.com',
+//   credentials: true
+// }));
+
 app.use(cors({
-  origin: 'http://localhost:5173', //luego cambiar a HTTPS y el nombre del domisnio 'https://midominio.com',
+  origin: process.env.CORS_ORIGIN || '*',
   credentials: true
-}));
+}))
 
 app.use(express.json())
 app.use(cookieParser())
@@ -21,8 +26,13 @@ app.use(cookieParser())
 app.use('/api/user', loginRoutes)
 app.use('/api/paradas', paradasRoutes)
 
-
-const PORT = Number(process.env.PORT) || 3000;
-app.listen(PORT, () => {
+// 🔹 Railway usa su propio PORT
+const PORT = Number(process.env.PORT) || 3000
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor backend corriendo en el puerto ${PORT}`)
 })
+
+// const PORT = Number(process.env.PORT) || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Servidor backend corriendo en el puerto ${PORT}`)
+// })
